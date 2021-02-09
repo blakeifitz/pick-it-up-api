8b506787cb97d216e41a8f634159362463362762
blakeifitz <blakeifitz@gmail.com>
2021-02-09 14:35:43 -0500

    first commit / user routes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'dev',
-  DATABASE_URL: process.env.DATABASE_URL || 'TEST_DATABASE_URL',
-  JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
-}
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://dunder-mifflin@localhost/spaced-repetition',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+};
--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
-const { expect } = require('chai');
-const supertest = require('supertest');
-
 process.env.TZ = 'UCT';
 process.env.NODE_ENV = 'test';
 process.env.JWT_SECRET = 'test-jwt-secret';
-process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+process.env.JWT_EXPIRY = '3m';
+
+require('dotenv').config();
+
+process.env.TEST_DB_URL =
+  process.env.TEST_DB_URL ||
+  'postgresql://blakefitzpatrick@localhost/spaced-repetition-test';
+
+const { expect } = require('chai');
+const supertest = require('supertest');
 
 global.expect = expect;
 global.supertest = supertest;
