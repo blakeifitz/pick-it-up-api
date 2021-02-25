1b31fb74724807bbd056be1aaeacfb20350ed349
blakeifitz <blakeifitz@gmail.com>
2021-02-25 16:58:03 -0500

    pg issues

--- src/config.js
+++ src/config.js
@@ -6,7 +6,4 @@ module.exports = {
     'postgresql://blakefitzpatrick@localhost/pick-it-up',
   JWT_SECRET: process.env.JWT_SECRET || 'pick-it-up-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  GOOGLE_APPLICATION_CREDENTIALS:
-    process.env.GOOGLE_APPLICATION_CREDENTIALS ||
-    '/Users/blakefitzpatrick/pick-it-up-897da-firebase-adminsdk-n2gyz-c9ad90c42c.json',
 };
