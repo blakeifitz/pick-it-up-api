90ef27172163c301f5a18ec5b3b87dceb17364ab
blakeifitz <blakeifitz@gmail.com>
2021-02-25 17:54:04 -0500

    removed firebase creds to stop heroku from crashing before migration

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ const admin = require('firebase-admin');
 
 // Initialize firebase admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(
-    '/Users/blakefitzpatrick/pick-it-up-897da-firebase-adminsdk-n2gyz-c9ad90c42c.json'
-  ),
+  //   credential: admin.credential.cert(
+  //     '/Users/blakefitzpatrick/pick-it-up-897da-firebase-adminsdk-n2gyz-c9ad90c42c.json'
+  //   ),
   storageBucket: 'pick-it-up-897da.appspot.com/',
 });
 // Cloud storage
