b0e3d6013108a0fc3a2bafc45ff1ebfa5c49eee8
blakeifitz <blakeifitz@gmail.com>
2021-02-28 18:22:00 -0500

    ran tests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const app = require('./app');
 const pg = require('pg');
-// pg.defaults.ssl = true;
+pg.defaults.ssl = true;
 const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
